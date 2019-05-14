09534a1bebd1403ef7f7fb91bf88d17bfc906d06
pedrobarata <pedrofelipebarata@gmail.com>
2019-05-14 14:54:59 -0300

    feat(4.11:00): Criação dos components e do Service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { TodoComponent } from './todo/todo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AuthComponent },
+  { path: 'todo', component: TodoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
