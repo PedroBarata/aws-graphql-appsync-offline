559239692f3d65e53c45724dfb1bbe57d3dbfe2a
pedrobarata <pedrofelipebarata@gmail.com>
2019-05-22 16:34:27 -0300

    feat(video 4): Offline feature

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { AppsyncService } from '../appsync.service';
+import { createTodo } from '../../graphql/mutations';
+import { listTodos } from '../../graphql/queries';
+
+import gql from 'graphql-tag';
+import { buildMutation } from 'aws-appsync';
+import { createTodoInput } from '../../graphql/inputs';
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +13,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
+  public allTodos = [];
 
-  constructor() { }
+  constructor(private appsync: AppsyncService) { }
 
   ngOnInit() {
+    this.appsync.hc().then(client => {
+      const observable = client.watchQuery({
+        query: gql(listTodos),
+        fetchPolicy: 'cache-and-network'
+      });
+      observable.subscribe(({ data }) => {
+        this.allTodos = data.listTodos.items;
+      });
+    });
   }
 
+  async createTodo(todoName) {
+    
+    if (todoName.value.length) {
+      /* Espera pelo client */
+      const client = await this.appsync.hc();
+
+      /* Mutation para salvar */
+      const result = await client.mutate(buildMutation(
+        client,
+        gql(createTodo),
+        {
+          inputType: gql(createTodoInput),
+          variables: {
+            input: {
+              name: todoName.value
+            }
+          }
+        },
+        _variables => [gql(listTodos)],
+        "Todo"
+      ));
+        console.log(result);
+        
+      this.allTodos.push(result.data.createTodo);
+      todoName.value = null;
+    }
+  }
 }
---
+++ src/graphql/inputs.ts
@@ -0,0 +1,4 @@
+export const createTodoInput = `input createTodoInput {
+    id: ID
+    name: String!
+  }`;
